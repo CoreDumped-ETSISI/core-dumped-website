da3eebe17ac708199bba7b7c12383eec6ea639fa
Rowiz49 <adrianpuyetm@gmail.com>
2023-09-04 10:25:56 +0200

    Add page to modify and create cards
    feat: Add page to modify and create cards

---
+++ src/routes/edit/+layout.svelte
@@ -0,0 +1,44 @@
+<!--Layout for editistration pages-->
+<script>
+  import { page } from "$app/stores";
+</script>
+
+<link rel="stylesheet" href="/global.css" />
+<!--Fontawesome is an icon kit, we should change to a Core Dumped account on production-->
+<head>
+  <script
+    src="https://kit.fontawesome.com/6f53737b3e.js"
+    crossorigin="anonymous"
+  >
+  </script>
+</head>
+<body>
+  <header>
+    <h2>Admin Mode</h2>
+    <div class="topnav">
+      <a
+        href="/edit/cartas"
+        class:active={$page.url.pathname === "/edit/cartas"}>Cartas</a
+      >
+      <a
+        href="/edit/personas"
+        class:active={$page.url.pathname === "/edit/personas"}>Personas</a
+      >
+    </div>
+  </header>
+
+  <div class="slot">
+    <slot />
+  </div>
+  <footer>
+    <div class="footer_wrap">
+      <a href="/"><img src="/logo.png" alt="Logo" class="logo" /></a>
+    </div>
+  </footer>
+</body>
+
+<style>
+  h2 {
+    color: #5fb030;
+  }
+</style>
