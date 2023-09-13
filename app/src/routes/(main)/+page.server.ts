9ad7e8fc47b411997dfc87f80b0b230cdf612d92
Rowiz49 <93596067+Rowiz49@users.noreply.github.com>
2023-09-13 12:59:05 +0200

    Update +page.server.ts
    Added 20 item limit to News page

--- app/src/routes/(main)/+page.server.ts
+++ app/src/routes/(main)/+page.server.ts
@@ -16,5 +16,5 @@ export interface Card {
 export const load: PageServerLoad = async ({ fetch, params }) => {
     const cards = await fetch(API_URI + '/cartas');
     let data: Array<Card> = await cards.json();
-    return { data };
+    return { data.slice(0,20) };
 };
