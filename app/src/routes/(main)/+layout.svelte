0c175aac151b3a254a185374a2feadda4a876cee
Rowiz49 <adrianpuyetm@gmail.com>
2023-09-24 11:11:37 +0200

    Add button for mobile menu navigation
    enhancement: Add button that shows the navbar in portrait mode

--- app/src/routes/(main)/+layout.svelte
+++ app/src/routes/(main)/+layout.svelte
@@ -4,12 +4,21 @@
   import "@fortawesome/fontawesome-free/css/all.min.css";
   let nav: HTMLDivElement;
   let showVerticalNav: boolean = false;
+  let header_style: string = "";
+  function changeVisibility() {
+    showVerticalNav = !showVerticalNav;
+    if (showVerticalNav) {
+      header_style = "box-shadow: none;";
+    } else {
+      header_style = "";
+    }
+  }
 </script>
 
 <link rel="stylesheet" href="/global.css" />
 
 <body>
-  <header>
+  <header style={header_style}>
     <a href="/"><img src="/logo.png" alt="Logo" class="logo" /></a>
     <div class="topnav" bind:this={nav}>
       <a href="/" class:active={$page.url.pathname === "/"}>Inicio</a>
@@ -23,7 +32,24 @@
         >Sobre nosotros</a
       >
     </div>
+    <button class="menu_button" on:click={changeVisibility}>
+      <i class="fa-solid fa-bars fa-2xl" style="color: #5fb030;" />
+    </button>
   </header>
+  {#if showVerticalNav}
+    <div class="hidden" bind:this={nav}>
+      <a href="/" class:active={$page.url.pathname === "/"}>Inicio</a>
+      <a href="/proyectos" class:active={$page.url.pathname === "/proyectos"}
+        >Proyectos</a
+      >
+      <a href="/eventos" class:active={$page.url.pathname === "/eventos"}
+        >Eventos</a
+      >
+      <a href="/about" class:active={$page.url.pathname === "/about"}
+        >Sobre nosotros</a
+      >
+    </div>
+  {/if}
 
   <div class="slot">
     <slot />
